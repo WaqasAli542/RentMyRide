cc27f77f3766021140ff03a661f47dde5fdfee94
WaqasAli542 <imwaqasali.542@gmail.com>
2017-05-26 16:27:51 +0500

    Setup

---
+++ RiderApplication/RiderApplication/Controllers/CarDetailsAPIController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RiderApplication.Models; 
+namespace RiderApplication.Controllers
+{
+    public class CarDetailsAPIController : ApiController
+    {
+        Database database = new Database();
+
+
+        //Get 
+        //With all parameters and here all parameters are optional
+        //
+        //http://localhost:27152/api/CarDetailsAPI/GetCarDetailsList?year=2016&&make=Acura&&model=RLX&&description=4d Sedan&&price=45900
+        public IEnumerable<CarDetail> GetCarDetailsList(String year=null,String make=null,String model=null,String description=null,double price=0.0)
+        {
+            List<CarDetail> lst = database.CarDetails.ToList();
+            if(year!=null)
+                lst = lst.Where(x => x.Year == year).ToList();
+
+            if(make!=null)
+                lst = lst.Where(x => x.Make == make).ToList();
+
+            if (model != null)
+                lst = lst.Where(x => x.Model == model).ToList();
+
+            if(description!=null)
+                lst = lst.Where(x => x.Description == description).ToList();
+         
+            if(price!=0.0)
+                lst = lst.Where(x => x.Price == price).ToList();
+
+            return lst;
+        }
+
+
+
+
+        public IEnumerable<CarOwner_Rider> GetCarOwner_RidersList(String year = null, String make = null, String model = null, String description = null, double price = 0.0,String area=null,int OwnerId=0)
+        {
+            List<CarOwner_Rider> lst = database.CarOwner_Rider.ToList();
+            if (year != null)
+                lst = lst.Where(x => x.CarDetail.Year == year).ToList();
+
+            if (make != null)
+                lst = lst.Where(x => x.CarDetail.Make == make).ToList();
+
+            if (model != null)
+                lst = lst.Where(x => x.CarDetail.Model == model).ToList();
+
+            if (description != null)
+                lst = lst.Where(x => x.CarDetail.Description == description).ToList();
+
+            if (price != 0.0)
+                lst = lst.Where(x => x.CarDetail.Price == price).ToList();
+
+            if (area != null)
+                lst = lst.Where(x => x.Area == area).ToList();
+
+            if (OwnerId!=0)
+                lst = lst.Where(x => x.OwnerId == OwnerId).ToList();
+            return lst;
+        }
+
+
+        public int saveCarOwner_Rider(CarOwner_Rider caror)
+        {
+            caror.IsAvailable = 1;
+            database.CarOwner_Rider.Add(caror);
+            return database.SaveChanges();
+        }
+    }
+}
---
+++ RiderApplication/RiderApplication/Controllers/HomeController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Excel = Microsoft.Office.Interop.Excel;
+using RiderApplication.Models; 
+
+namespace RiderApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        Database database = new Database();
+        public ActionResult Index()
+        {
+            //Excel.Application xlApp = new Excel.Application();
+            //Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\System A\Desktop\data\NewCarMarketValue.csv");
+            //Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            //Excel.Range xlRange = xlWorksheet.UsedRange;
+ 
+            //int rowCount = xlRange.Rows.Count;
+            //int colCount = xlRange.Columns.Count;
+            //for (int i = 4; i <= rowCount; i++)
+            //{ 
+            //    CarDetail cardetail= new CarDetail();
+
+            //    if ((xlRange.Cells[i, 2] != null && xlRange.Cells[i, 2].Value2 != null) && (xlRange.Cells[i, 3] != null && xlRange.Cells[i, 3].Value2 != null) && (xlRange.Cells[i, 4] != null && xlRange.Cells[i, 4].Value2 != null) && (xlRange.Cells[i, 5] != null && xlRange.Cells[i, 5].Value2 != null) && (xlRange.Cells[i, 6] != null && xlRange.Cells[i, 6].Value2 != null))
+            //    {
+            //        cardetail.Year = xlRange.Cells[i, 2].Value2.ToString();
+            //        cardetail.Make = xlRange.Cells[i, 3].Value2.ToString();
+            //        cardetail.Model = xlRange.Cells[i, 4].Value2.ToString();
+            //        cardetail.Description = xlRange.Cells[i, 5].Value2.ToString();
+
+            //        String temp = xlRange.Cells[i, 6].Value2.ToString();
+            //        temp = temp.Replace("$", ""); temp = temp.Replace(",", "");
+            //        cardetail.Price = (float)Convert.ToDouble(temp);
+            //        database.CarDetails.Add(cardetail);
+            //        database.SaveChanges();
+            //    }
+            //}
+            return View();
+
+      }
+
+        //
+        // GET: /Home/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Home/Delete/5
+
+       
+    }
+}
